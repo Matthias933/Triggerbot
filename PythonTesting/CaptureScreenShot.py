85e4b0064eba1c803e85309dd6e8b952e6fdceff
Matthias933 <MSBR@bachmann.at>
2024-10-01 18:59:24 +0200

    small changes still doesnt really work

--- PythonTesting/CaptureScreenShot.py
+++ PythonTesting/CaptureScreenShot.py
@@ -6,9 +6,12 @@ import serial
 import threading
 import time
 
-#TIP user rgb color for red: 131,29,25
+# TIP: User RGB color for red: 131,29,25
 
 class TriggerBotApp:
+
+    ser = serial.Serial('COM5', 9600)
+
     def __init__(self, master):
         self.master = master
         master.title("Trigger Bot Control")
@@ -21,15 +24,15 @@ class TriggerBotApp:
         self.label.pack()
 
         self.red_entry = tk.Entry(master, fg="#EEEEEE", bg="#222831")
-        self.red_entry.insert(0, "0")  # Default value
+        self.red_entry.insert(0, "131")  # Default value for red
         self.red_entry.pack()
 
         self.green_entry = tk.Entry(master, fg="#EEEEEE", bg="#222831")
-        self.green_entry.insert(0, "255")  # Default value
+        self.green_entry.insert(0, "29")  # Default value for green
         self.green_entry.pack()
 
         self.blue_entry = tk.Entry(master, fg="#EEEEEE", bg="#222831")
-        self.blue_entry.insert(0, "0")  # Default value
+        self.blue_entry.insert(0, "25")  # Default value for blue
         self.blue_entry.pack()
 
         # Detection status label
@@ -47,9 +50,7 @@ class TriggerBotApp:
         self.status_label = tk.Label(master, text="Status: Stopped", fg="#EEEEEE", bg="#222831")
         self.status_label.pack()
 
-        #TODO implement me
-        #self.ser = serial.Serial('COM3', 115200)
-        time.sleep(2)
+        time.sleep(2)  # Allow time for serial connection
 
     def start_trigger_bot(self):
         if not self.running:
@@ -63,8 +64,14 @@ class TriggerBotApp:
         self.detection_status_label.config(text="Detection Status: No Enemy Detected", fg="red", bg="#222831")  # Reset status
 
     def run_bot(self):
+        screen_width, screen_height = pyautogui.size()
+        print(screen_width, screen_height)
+        center_x = screen_width // 2
+        center_y = screen_height // 2
+        region_size = 25  # Size of the region around the center to check for color
+
         while self.running:
-            screenshot = pyautogui.screenshot()
+            screenshot = pyautogui.screenshot(region=(center_x - region_size, center_y - region_size, region_size * 2, region_size * 2))
             screenshot_np = np.array(screenshot)
 
             # Get RGB values from entry fields
@@ -74,17 +81,19 @@ class TriggerBotApp:
             color_lower = np.array([red - 10, green - 10, blue - 10])
             color_upper = np.array([red + 10, green + 10, blue + 10])
 
+            # Check if the specified color is within the region around the center
             mask = np.all(np.logical_and(screenshot_np >= color_lower, screenshot_np <= color_upper), axis=-1)
-            coordinates = np.argwhere(mask)
-
-            if coordinates.size > 0:
-                first_coord = coordinates[0]
-
-                self.ser.write(f"{first_coord[0]},{first_coord[1]}\n".encode('utf-8'))
-                
+            if np.any(mask):
+                # If the color is found, trigger the bot
+                print("Enemy detected in center region!")
+                self.ser.write(b"1\n")
                 self.detection_status_label.config(text="Detection Status: Enemy Detected!", fg="green", bg="#222831")
             else:
+                self.ser.write(b"0\n")
+                print("No Enemy detected in center region!")
                 self.detection_status_label.config(text="Detection Status: No Enemy Detected", fg="red", bg="#222831")
+
+            time.sleep(0.5)  # Adjust frequency of checking
 
 if __name__ == "__main__":
     root = tk.Tk()
